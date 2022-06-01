4164d0159dc4ce945bb28172bf079bf7c35cf016
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-31 19:40:08 -0500

    resuelto 1 funciona

--- resueltos/resuelto1.py
+++ resueltos/resuelto1.py
@@ -1,13 +1,22 @@
 def esEscalar(m):
-  d = m[0][0]
-  for i in range(len(m)):
-    for j in range(len(m)):
-      if i != j:
-        if m[i][j] != 0:
-          print(m[i][j])
-          return False
-      elif m[i][j] != d:
-        print(m[i][j])
-        return False
-  return True
+	d = m[0][0]
+	for i in range(len(m)):
+		for j in range(len(m)):
+			if i != j:
+				if m[i][j] != 0:
+					print(m[i][j])
+					return False
+			elif m[i][j] != d:
+				print(m[i][j])
+				return False
+	return True
 
+matriz = [[3,0,0,0],[0,3,0,0],[0,0,3,0],[0,0,0,3]]
+
+
+
+
+if esEscalar(matriz):
+	print("Es matriz ...")
+else:
+	print("No es ...")

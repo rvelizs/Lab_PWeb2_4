d40a3371259492ce05ab459e7b8ec5987018acdf
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-02 17:03:57 -0500

    Propuesto figuras

--- propuestos/src/Tarea-del-Ajedrez/Ejercicio2f.py
+++ propuestos/src/Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+# Para la primera mitad del tablero
+parte1A = square.join(square.negative())
+parte2A = parte1A.horizontalRepeat(4)
+
+# Para la mitad faltante
+parteB = parte2A.verticalRepeat(2)
+
+dibFinal = Picture(parte2A.img + parteB.img)

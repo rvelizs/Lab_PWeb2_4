036c41f2339f79abc9e91276befbeed09a57abef
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-02 16:56:25 -0500

    Propuesto d v2 completo

--- propuestos/src/Tarea-del-Ajedrez/Ejercicio2d.py
+++ propuestos/src/Tarea-del-Ajedrez/Ejercicio2d.py
@@ -1,4 +1,7 @@
 from interpreter import draw
 from chessPictures import *
 
-fil1 = square.join(square.negative())
+parte1 = square.join(square.negative())
+parte2 = parte1.horizontalRepeat(4)
+
+draw(Picture(parte2.img))

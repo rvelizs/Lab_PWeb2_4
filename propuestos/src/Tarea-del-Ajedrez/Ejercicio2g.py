5a507423e6a5debf54ae9462d95cc59ebc8f046b
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-03 09:31:54 -0500

    ejercicio g completo, comentado

--- propuestos/src/Tarea-del-Ajedrez/Ejercicio2g.py
+++ propuestos/src/Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,12 +1,28 @@
 from interpreter import draw
 from chessPictures import *
 
-torre1 = rock.up(square)
-caballo1 = knight.up(square.negative())
-alfil1 = bishop.up(square)
-reina = queen.up(square.negative())
-rey = king.up(square)
-alfil2 = bishop.up(square.negative())
-caballo2 = knight.up(square)
-torre2 = rock.up(square.negative())
+#por separado
+filaPGrandes = rock.join(knight.join(bishop.join(queen.join(king.join(bishop.join(knight.join(rock)))))))
+filaPPeon = pawn.horizontalRepeat(8)
 
+blancas = Picture(filaPPeon.img + filaPGrandes.img)
+negras = Picture(filaPGrandes.img + filaPPeon.img).negative()
+
+# Partes del tablero
+parte1A = square.join(square.negative())
+parte2A = parte1A.horizontalRepeat(4)
+parte1B = parte2A.negative()
+#
+parcial = Picture(parte2A.img + parte1B.img)
+#                                   ............Parte 2
+total = parcial.verticalRepeat(2)
+
+# De las negras : negras sobre parcial..........Parte 1
+eNegras = negras.up(parcial)
+# De las blancas : blancas sobre parcial........Parte 3
+eBlancas = blancas.up(parcial)
+
+# Armando el tablero:
+tablero = Picture(eNegras.img + total.img + eBlancas.img)
+
+draw(tablero)

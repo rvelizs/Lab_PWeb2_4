065c4b01bf28956c7ea95b21075d987a8ec3cfe3
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-02 22:29:27 -0500

    Intento de ejericio f

--- propuestos/src/Tarea-del-Ajedrez/Ejercicio2g.py
+++ propuestos/src/Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,2 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+
+torre1 = rock.up(square)
+caballo1 = knight.up(square.negative())
+alfil1 = bishop.up(square)
+reina = queen.up(square.negative())
+rey = king.up(square)
+alfil2 = bishop.up(square.negative())
+caballo2 = knight.up(square)
+torre2 = rock.up(square.negative())
+
